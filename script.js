084e8a0c5a00410624e9002947abf26d1315ba67
gurkandizen <gurkandizen17@gmail.com>
2026-01-05 00:32:05 +0300

    I have completed the Customer Reviews (References) section and the Swiper integration.

--- script.js
+++ script.js
@@ -7,4 +7,36 @@ menuOpenBtn.addEventListener("click", () => {
 });
 
 // Kapat düğmesine tıklandığında menüyü kapatır
-menuCloseBtn.addEventListener("click", () => menuOpenBtn.click())
+menuCloseBtn.addEventListener("click", () => menuOpenBtn.click());
+
+const swiper = new Swiper('.slide-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+//   Resposive Breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2 
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
+});
